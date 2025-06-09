057ebbcaf79b485d173f3171ff8b9815e5b8a55e
lazarev <lazarev@moitw.ru>
2025-06-09 09:23:31 +0300

    Employer fix

--- src/main/java/org/example/spu/Employer.java
+++ src/main/java/org/example/spu/Employer.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 
 import java.util.Date;
 
@@ -14,23 +15,30 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PROTECTED, force=true)
 public class Employer {
     
+
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "employer_generator")
+	@SequenceGenerator(name = "employer_generator", sequenceName = "employer_id_seq", allocationSize = 1)
     private Long id;
 
-    @NotNull
+    // @NotNull
     @Column(length=200, nullable=false, unique=false)
     private String name;
 
@@ -39,9 +47,13 @@ public class Employer {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @NotNull
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinTable(name = "department")
-    @JoinColumn(name = "department_id", referencedColumnName = "id")
+    // @ManyToOne(cascade = CascadeType.ALL)
+    // @JoinTable(name = "department")
+    // @JoinColumn(name = "department_id", referencedColumnName = "id")
+    // private Department department;
+
+        // @ManyToOne(fetch = FetchType.LAZY)
+        @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 }
